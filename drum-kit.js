4b70bf45bdea467d33d76dcd43c50c7d3b2d3326
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2020-07-16 21:28:26 -0400

    CSS editing background color and individual keys

--- drum-kit.js
+++ drum-kit.js
@@ -1,7 +1,6 @@
 function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(audio);
     if(!audio) {
         return;
     }
